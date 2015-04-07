46d35042d877e4ece9c43ec15615450862ff2f78
Adalyn Johnson <johnsoaa@rose-hulman.edu>
2015-04-06 21:57:11 -0400

    completed lab 4

--- ExpediaTest/CarTest.cs
+++ ExpediaTest/CarTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Expedia;
+using System.Collections.Generic;
 using Rhino.Mocks;
 
 namespace ExpediaTest
@@ -44,11 +45,46 @@ namespace ExpediaTest
 			Assert.AreEqual(10*7*.8, target.getBasePrice());
 		}
 		
-		[TestMethod]
-		[ExpectedException(typeof(ArgumentOutOfRangeException))]
-		public void TestThatCarThrowsOnBadLength()
-		{
-			new Car(-5);
-		}
+
+        [TestMethod]
+        public void TestThatCarGetCarLocation()
+        {
+            String carLocation ="";
+            IDatabase mockDB = mocks.StrictMock<IDatabase>();
+            List<Int32> Cars = new List<int>();
+
+            Expect.Call(mockDB.getCarLocation(5)).Return(carLocation);
+
+            mocks.ReplayAll();
+
+            targetCar.Database = mockDB;
+
+            String result;
+            result = targetCar.getCarLocation(5);
+            Assert.AreEqual(carLocation, result);
+            mocks.VerifyAll();
+        }
+        [TestMethod]
+        public void TestThatCarMileage()
+        {
+            Int32 carMiles= 55000;
+            IDatabase mockDB = mocks.StrictMock<IDatabase>();
+
+            Int32 Miles = 55000;
+            Expect.Call(mockDB.Miles).PropertyBehavior();
+            mocks.ReplayAll();
+            targetCar = ObjectMother.BMW(); //showing use of ObjectMother
+            mockDB.Miles = Miles;
+            targetCar.Database = mockDB;
+            Int32 result = targetCar.Mileage;
+            Assert.AreEqual(carMiles, result);
+            mocks.VerifyAll();
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatCarThrowsOnBadLength()
+        {
+            new Car(-5);
+        }
 	}
 }
--- ExpediaTest/HotelTest.cs
+++ ExpediaTest/HotelTest.cs
@@ -55,23 +55,49 @@ namespace ExpediaTest
 		}
         [TestMethod]
         public void TestThatHotelDoesGetRoomOccupantFromDatabase() {
-            IDatabase mockDB = mocks.StrictMock<IDatabase>();
+            IDatabase mockDB = mocks.DynamicMock<IDatabase>();
             String roomOccupant = "Whale Rider";
             String anotherRoomOccupant = "Raptor Wrangler";
-            Expect.Call(mockDB.getRoomOccupant(24)).Return(roomOccupant);
-            Expect.Call(mockDB.getRoomOccupant(1025)).Return(anotherRoomOccupant);
+            //Expect.Call(mockDB.getRoomOccupant(24)).Return(roomOccupant);
+            //Expect.Call(mockDB.getRoomOccupant(1025)).Return(anotherRoomOccupant);
+            using (mocks.Ordered())
+            {
+                Expect.Call(mockDB.getRoomOccupant(24)).Return(roomOccupant);
+                Expect.Call(mockDB.getRoomOccupant(1025)).Return(anotherRoomOccupant);
+            }
             mocks.ReplayAll();
 
             Hotel target = new Hotel(10);
             target.Database = mockDB;
 
+            mockDB.Stub(x => x.getRoomOccupant(Arg<int>.Is.Equal(25))).Return("Empty room");
+
             String result;
-            result = target.getRoomOccupant(1025);
-            Assert.AreEqual(anotherRoomOccupant, result);
             result = target.getRoomOccupant(24);
             Assert.AreEqual(roomOccupant, result);
+            result = target.getRoomOccupant(1025);
+            Assert.AreEqual(anotherRoomOccupant, result);
+            result = target.getRoomOccupant(25);
+            Assert.AreEqual("Empty room", result);
             mocks.VerifyAll();
             
+        }
+        [TestMethod()]
+        public void TestThatHotelDoesGetRoomCoutFromDatabase() {
+            IDatabase mockDatabase = mocks.StrictMock<IDatabase>();
+            List<Int32> Rooms = new List<int>();
+            for (var i = 0; i < 100; i++)
+            {
+                Rooms.Add(i);
+            }
+            Expect.Call(mockDatabase.Rooms).PropertyBehavior();
+            mocks.ReplayAll();
+            mockDatabase.Rooms = Rooms;
+            var target = new Hotel(10);
+            target.Database = mockDatabase;
+            int roomCount = target.AvailableRooms;
+            Assert.AreEqual(roomCount, Rooms.Count);
+            mocks.VerifyAll();
         }
 	}
 }
